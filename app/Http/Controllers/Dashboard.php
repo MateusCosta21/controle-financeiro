ff4f9097a980089208a6ca7aedbf82f491030dae
mateuscosta <joao.santos@fabricainfo.com>
2023-11-20 18:46:55 -0300

    [feature] Where por usuario

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -6,18 +6,23 @@ use App\Models\Despesa;
 use App\Models\Receita;
 use Illuminate\Http\Request;
 Use App\Models\TipoDespesa;
+use Illuminate\Support\Facades\Auth;
+
 
 class Dashboard extends Controller
 {
     public function getData($month) {
-        $receitas = Receita::whereRaw("EXTRACT(MONTH FROM data_Entrada) = ?", [$month])->get();
-        $despesas = Despesa::whereRaw("EXTRACT(MONTH FROM data_vencimento) = ?", [$month])->get();
-        $despesasComNomes = $despesas->map(function ($despesa) {
-        $nomeDespesa = $despesa->tipoDespesa->nome_despesa;
+        $idUsuario = Auth::id();
+        $receitas = Receita::whereRaw("EXTRACT(MONTH FROM data_Entrada) = ?", [$month])
+                            ->where('id_usuario', $idUsuario)
+                            ->get();
     
-            // Adiciona o nome_despesa aos atributos da despesa
+        $despesas = Despesa::whereRaw("EXTRACT(MONTH FROM data_vencimento) = ?", [$month])
+                            ->where('id_usuario', $idUsuario)
+                            ->get();
+        $despesasComNomes = $despesas->map(function ($despesa) {
+            $nomeDespesa = $despesa->tipoDespesa->nome_despesa;
             $despesa->nome_despesa = $nomeDespesa;
-
             return $despesa;
         });
     
@@ -26,12 +31,11 @@ class Dashboard extends Controller
     
         $result = [
             'receitas' => $receitas,
-            'despesas' => $despesasComNomes, // Use o array com os nomes adicionados
+            'despesas' => $despesasComNomes,
             'soma_valores_receitas' => $somaValoresReceitas,
             'soma_valores_despesas' => $somaValoresDespesas,
         ];
     
         return response()->json(['data' => $result]);
-        
     }
 }
