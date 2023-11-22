72121a9569657e0fde7e3cd0f5606d28bebb3490
mateuscosta <joao.santos@fabricainfo.com>
2023-11-22 18:32:21 -0300

    [feature] Inicia mes janeiro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,6 @@
                 <select class="form-control" id="selectMonth" onchange="loadData()">
                     <option value="1">Janeiro</option>
                     <option value="2">Fevereiro</option>
-                    <option value="2">Março</option>
                     <option value="3">Março</option>
                     <option value="4">Abril</option>
                     <option value="5">Maio</option>
@@ -30,7 +29,7 @@
         <!-- Content Row -->
         <div class="row">
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-md-4 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
@@ -47,7 +46,7 @@
                 </div>
             </div>
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-md-4 mb-4">
                 <div class="card border-left-success shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
@@ -64,7 +63,7 @@
                 </div>
             </div>
             <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
+            <div class="col-md-4 mb-4">
                 <div class="card border-left-warning shadow h-100 py-2" id="saldoCard">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
@@ -80,24 +79,7 @@
                     </div>
                 </div>
             </div>
-            <!-- Pending Requests Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-warning shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                    Pending Requests</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-comments fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    
     </div>
 
     <div class="modal fade" id="confirmarPagamentoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
@@ -270,6 +252,13 @@
                     idDespesaParaConfirmar = null;
                 }
             }
+
+            window.addEventListener('load', function () {
+            // Define o mês padrão para dezembro (12)
+            document.getElementById("selectMonth").value = "1";
+            // Chama a função loadData() para carregar dados de dezembro
+            loadData();
+        });
         </script>
 
         <!-- /.container-fluid -->
