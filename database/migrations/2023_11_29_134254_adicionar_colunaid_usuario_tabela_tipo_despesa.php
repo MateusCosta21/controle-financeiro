626d8ecaafb15f262b9d8a3bc5a041a075b7ea63
mateuscosta <joao.santos@fabricainfo.com>
2023-11-29 10:48:22 -0300

    [feature] Tipos
    Adicionando id_usuario aos tipos

---
+++ database/migrations/2023_11_29_134254_adicionar_colunaid_usuario_tabela_tipo_despesa.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tipo_despesas', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_usuario')->nullable(); 
+            $table->foreign('id_usuario')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tipo_despesas', function (Blueprint $table) {
+            $table->dropForeign(['id_usuario']);
+            $table->dropColumn('id_usuario');
+        });
+    }
+};
