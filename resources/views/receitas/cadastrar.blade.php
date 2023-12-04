eccb692b6f4b93a7abeb1fce46c63589e3750c97
mateuscosta <joao.santos@fabricainfo.com>
2023-12-04 19:18:06 -0300

    validacao funcionando novamente

--- resources/views/receitas/cadastrar.blade.php
+++ resources/views/receitas/cadastrar.blade.php
@@ -7,11 +7,15 @@
                 {{ session('success') }}
             </div>
         @endif
-        @if (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
             <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 class="h3 mb-0 text-gray-800">Receitas</h1>
